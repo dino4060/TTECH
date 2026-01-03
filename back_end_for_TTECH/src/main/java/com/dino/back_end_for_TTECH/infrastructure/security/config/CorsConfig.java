f2a7cc21701c6afd687d2dd5afc4d6db246ecc44
tn9th8 <tn9th8@gmail.com>
2026-01-03 15:11:48 +0700

    fix cors

--- back_end_for_TTECH/src/main/java/com/dino/back_end_for_TTECH/infrastructure/security/config/CorsConfig.java
+++ back_end_for_TTECH/src/main/java/com/dino/back_end_for_TTECH/infrastructure/security/config/CorsConfig.java
@@ -10,36 +10,34 @@ import java.util.Arrays;
 
 @Configuration
 public class CorsConfig {
-    /**
-     * Specify the services that are allowed to connect to our servers.
-     * Include:
-     * - Domain URLs
-     * - Http methods
-     * - Http headers
-     * - Http cookies
-     */
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration corsConfig = new CorsConfiguration();
-        corsConfig.setAllowedOrigins(
-                Arrays.asList(
-                        "http://localhost:3000", "buyer-deal.vercel.app",
-                        "http://localhost:3001", "seller-deal.vercel.app",
-                        "http://localhost:3002", "admin-deal.vercel.app"
-                ));
+  /**
+   * Specify the services that are allowed to connect to our servers.
+   * Include:
+   * - Domain URLs
+   * - Http methods
+   * - Http headers
+   * - Http cookies
+   */
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration corsConfig = new CorsConfiguration();
+    corsConfig.setAllowedOrigins(
+        Arrays.asList(
+            "http://localhost:3000",
+            "https://ttech-studio.vercel.app"));
 
-        corsConfig.setAllowedMethods(
-                Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+    corsConfig.setAllowedMethods(
+        Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
 
-        corsConfig.setAllowedHeaders(
-                Arrays.asList("Authorization", "Content-Type", "Accept", "x-no-retry"));
+    corsConfig.setAllowedHeaders(
+        Arrays.asList("Authorization", "Content-Type", "Accept", "x-no-retry"));
 
-        corsConfig.setAllowCredentials(true); //allow client send request attached cookies
+    corsConfig.setAllowCredentials(true); // allow client send request attached cookies
 
-        corsConfig.setMaxAge(3600L); //pre-flight request time có thể cache (tính theo seconds)
+    corsConfig.setMaxAge(3600L); // pre-flight request time có thể cache (tính theo seconds)
 
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", corsConfig); //apply the policy to all routes
-        return source;
-    }
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", corsConfig); // apply the policy to all routes
+    return source;
+  }
 }
